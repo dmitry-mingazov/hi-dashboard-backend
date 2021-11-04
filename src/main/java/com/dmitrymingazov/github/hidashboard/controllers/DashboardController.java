f96ee207f6f08d6b551cdc074c4461d99a646f3f
Dmitry Mingazov <mingazov@email.com>
2021-11-04 17:38:43 +0100

    Secure endpoints with jwt handling

--- src/main/java/com/dmitrymingazov/github/hidashboard/controllers/DashboardController.java
+++ src/main/java/com/dmitrymingazov/github/hidashboard/controllers/DashboardController.java
@@ -3,6 +3,8 @@ package com.dmitrymingazov.github.hidashboard.controllers;
 import com.dmitrymingazov.github.hidashboard.data.Dashboard;
 import com.dmitrymingazov.github.hidashboard.service.DashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,14 +16,17 @@ public class DashboardController {
     @Autowired
     private DashboardService dashboardService;
 
-    @GetMapping()
+    @GetMapping("/all")
     public List<Dashboard> findAll() { return dashboardService.findAll();}
 
     @GetMapping("/{id}")
     public Dashboard findById(@PathVariable String id) { return dashboardService.findById(id);}
 
-    @GetMapping("/of/{user}")
-    public List<Dashboard> findAllByUser(@PathVariable String user) { return dashboardService.findAllByUser(user);}
+    @GetMapping()
+    public List<Dashboard> findAllByUser(Authentication auth) {
+        System.out.println(auth.getName());
+        return dashboardService.findAllByUser(auth.getName());
+    }
 
     @PostMapping()
     public Dashboard save(@RequestBody Dashboard dashboard) { return dashboardService.save(dashboard); }
